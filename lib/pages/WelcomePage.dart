eeafbaaffdf4f2fa56f5a1d4a07129b6239cafa7
Nikhil Narayanan <javakingxi@gmail.com>
2023-04-08 16:00:09 +0530

    ✨ Add hyperlinks to Welcome message

--- lib/pages/WelcomePage.dart
+++ lib/pages/WelcomePage.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_automata/pages/InitializationPage.dart';
 import 'package:flutter_automata/util/DialogManager.dart';
+import 'package:flutter_automata/util/custom_exceptions.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WelcomePage extends StatelessWidget {
   static const int minRows = 2;
@@ -15,6 +18,10 @@ class WelcomePage extends StatelessWidget {
   int ub;
   int lb;
   int ress;
+  static const String CAinfo =
+      "https://en.wikipedia.org/wiki/Cellular_automaton"; //URL to be launched for viewing cellular automaton info
+  static const String GOLinfo =
+      "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"; //URL to be lauched for game of life info
   // static const String welcomeMessage =
   //     "Welcome to Game of Life !\n\n\nA Cellular Automaton (CA) is a collection of cells arranged in a grid of specified shape, such that each cell changes state as a function of time, according to a defined set of rules driven by the states of neighboring cells.\n\nGame of Life is a particular kind of cellular automaton, introduced by John Connway in 1970, consisting of 2-state cells(dead or alive).\n\nThese automatons form self-generative patterns, and have applications in fields like cryptography and traffic simulation. \nHere, you can create your own system of living cells with your own set of rules!";
   static const String mssg1 = "Welcome to Game of Life !\n\n\nA ";
@@ -75,18 +82,22 @@ class WelcomePage extends StatelessWidget {
                         ),
                       ),
                       TextSpan(
-                        text: hyperlink1,
-                        style: GoogleFonts.sourceCodePro(
-                          color: Colors.transparent,
-                          shadows: [
-                            const Shadow(
-                                offset: Offset(0, -1), color: Colors.cyanAccent)
-                          ],
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.cyanAccent,
-                          fontSize: 17,
-                        ),
-                      ),
+                          text: hyperlink1,
+                          style: GoogleFonts.sourceCodePro(
+                            color: Colors.transparent,
+                            shadows: [
+                              const Shadow(
+                                  offset: Offset(0, -1),
+                                  color: Colors.cyanAccent)
+                            ],
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.cyanAccent,
+                            fontSize: 17,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              launchURL(CAinfo, context);
+                            }),
                       TextSpan(
                         text: mssg2,
                         style: GoogleFonts.sourceCodePro(
@@ -106,6 +117,10 @@ class WelcomePage extends StatelessWidget {
                           decorationColor: Colors.cyanAccent,
                           fontSize: 17,
                         ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            launchURL(GOLinfo, context);
+                          },
                       ),
                       TextSpan(
                         text: mssg3,
@@ -147,5 +162,18 @@ class WelcomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+//launches information url.
+launchURL(String urlInp, BuildContext context) async {
+  Uri url = Uri.parse(urlInp);
+
+  if (await launchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    // ignore: use_build_context_synchronously
+    DialogManager.openInfoDialog(
+        details: "Could not launch $url", context: context);
   }
 }
--- lib/util/custom_exceptions.dart
+++ lib/util/custom_exceptions.dart
@@ -2,3 +2,8 @@ class MatrixOutOfBoundsException {
   String message;
   MatrixOutOfBoundsException({required this.message});
 }
+
+class FailedUrlLauchException {
+  String message;
+  FailedUrlLauchException({required this.message});
+}
